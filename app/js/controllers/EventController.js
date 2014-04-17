4b2a8c4fc82d92bc3a063c8cd0a294235cde49d9
Jamshid A <jamshid.asatillayev@gmail.com>
2014-04-16 21:56:25 -0700

    date filter added

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,9 +1,14 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function ($scope, eventData) {
+    function ($scope,$sce, eventData) {
         $scope.sortorder = 'name';
+//        $scope.snippet="<span style='color: red;'>hello world snippet</span>" ;
+//        $scope.snippetSafe= $sce.trustAsHtml($scope.snippet) ;
 
+        $scope.toJsDate= function(strDate) {
+            return new Date(strDate);
+        };
         $scope.upVoteSession = function (session) {
             session.upVoteCount++;
         };
